efaee54eac51df5c417c008c1a214c556fc551e4
K.McMurray <biofart@pm.me>
2024-08-08 16:31:36 -0400

    Fixed
    qrcode works. comments.

--- qrcode.py
+++ qrcode.py
@@ -1,7 +1,21 @@
+"""
+QR-URL TOOL
+AUTHOR: mcuser
+DATE: 08/2024
+Version: 1
+pip install pyqrcode pypng pillow
+"""
+
 import pyqrcode
 from PIL import Image
+
 print("Generate QR code")
 link = input("Enter text/url: ")
+
+# Generate QR code
 qr_code = pyqrcode.create(link)
-qr_code.png("/path/to/file   /QRCode.png", scale=5)
-Image.open("QRCode.png")
+qr_code.png("QRCode.png", scale=5)
+
+# Open and display the QR code image
+image = Image.open("QRCode.png")
+image.show()  # Use .show() to display the image
